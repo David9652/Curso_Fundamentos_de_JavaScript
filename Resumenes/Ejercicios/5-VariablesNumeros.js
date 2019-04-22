95b4fb68495c8217f1d95a0d5a95fd1e5bfd2627
David Angel <carlosdavid_9652@hotmail.com>
2019-04-22 15:43:55 -0500

    Resumen Sesión 5 - Variables: Números

---
+++ Resumenes/Ejercicios/5-VariablesNumeros.js
@@ -0,0 +1,12 @@
+var edad = 23
+edad += 1
+
+var peso = 55
+peso -= 2
+
+var precioVino = 200.3
+var total = Math.round(precioVino * 3 * 100)/100
+var totalStr = total.toFixed(3)
+var total2 = parseFloat(total)
+
+console.log(total2)
