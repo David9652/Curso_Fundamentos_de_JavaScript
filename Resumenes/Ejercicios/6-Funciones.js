b6060a99a5455f8d0203ee67672d6c6c7e13d154
David Angel <carlosdavid_9652@hotmail.com>
2019-04-22 16:34:18 -0500

    Resumen Sesión 6 - Funciones

---
+++ Resumenes/Ejercicios/6-Funciones.js
@@ -0,0 +1,11 @@
+var nombre = 'David', edad = 23
+
+function imprimirEdad(n,e){
+    console.log(`${n} tiene ${e} años.`)
+}
+
+imprimirEdad(nombre,edad)
+imprimirEdad("Daniel",16)
+imprimirEdad("Andrés",24)
+
+// Si quieres saber el tipo de dato de tu variable puedes usar la función definida typeof()
