d194f11761b7e87c2b5c7f7eeff2db5c30b2e19a
David Angel <carlosdavid_9652@hotmail.com>
2019-04-22 13:57:11 -0500

    Resumen Sesión 3 - Variables

---
+++ Resumenes/Ejercicios/3-Variables.js
@@ -0,0 +1,10 @@
+var nombre = "David";
+var apellido = "Angel"
+var edad = 23
+console.log('Hola '+ nombre + " " + apellido);
+console.log('Tengo ' + edad + " años");
+/*
+var = Una variable global
+const = Una varible que no se puede cambiar su contenido
+let = Palabra reservada que permite declarar variables, limitando su alcance
+*/
