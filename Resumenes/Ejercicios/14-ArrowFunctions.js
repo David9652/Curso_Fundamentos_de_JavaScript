76f2f196fdbcef61646c505b143fb3b7df5a5921
David Angel <carlosdavid_9652@hotmail.com>
2019-04-23 17:09:24 -0500

    Resumen Sesión 14 - Arrow Functions

---
+++ Resumenes/Ejercicios/14-ArrowFunctions.js
@@ -0,0 +1,86 @@
+var johnny = {
+  nombre: 'Johnny',
+  apellido: 'Ber',
+  edad: 16,
+  ingeniero: true,
+  cosinero: false,
+  canante: false,
+  dj: false,
+  guitarista: true,
+  drone: false,
+}
+
+function imprimirProfesiones(persona){
+  console.log(`${persona.nombre}`)
+
+  if (persona.ingeniero) {
+    console.log('es: Ingeniero')
+  } else {
+    console.log('No es Ingeniero')
+  }
+
+  if (persona.cosinero) {
+    console.log('es: Cosinero')
+  } else {
+    console.log('No es Cosinero')
+  }
+
+  if (persona.cantante) {
+    console.log('es: Cantante')
+  } else {
+    console.log('No es Cantante')
+  }
+
+  if (persona.dj) {
+    console.log('es: Dj')
+  } else {
+    console.log('No es Dj')
+  }
+
+  if (persona.guitarista) {
+    console.log('es: Guitarista')
+  } else {
+    console.log('No es Guitarista')
+  }
+
+  if (persona.drone) {
+    console.log('es: Piloto de Drone')
+  } else {
+    console.log('No pilotea drones')
+  }
+}
+
+imprimirProfesiones(johnny)
+
+const MAYOR_EDAD = 18
+
+/*
+Constante asignada como función
+const esMayorDeEdad = function (persona){
+    return persona.edad >= MAYOR_EDAD
+}
+Arrow Function
+const esMayorDeEdad = persona => persona.edad >= MAYOR_EDAD
+*/
+
+const esMayorDeEdad = ({edad}) => edad >= MAYOR_EDAD
+
+function imprimirSiEsMayorDeEdad(persona){
+    console.log(`${persona.nombre} tiene ${persona.edad},`)
+  
+    if (esMayorDeEdad(persona)) {
+      console.log('es mayor de edad')
+    }
+    else{
+        console.log('es menor de edad')
+    }
+  }
+  
+function permitirAcceso (persona){
+    if(!esMayorDeEdad(persona)){
+        console.log("ACCESO DENEGADO")
+    }
+}
+
+imprimirSiEsMayorDeEdad(johnny)
+permitirAcceso(johnny)
